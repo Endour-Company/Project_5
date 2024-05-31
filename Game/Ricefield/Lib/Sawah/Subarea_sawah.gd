595505e3a21411558c63c622245612be0b239609
shizucc <syifanaalfi@gmail.com>
2024-05-31 11:48:27 +0700

    sawah logic on progess

--- Game/Ricefield/Lib/Sawah/Subarea_sawah.gd
+++ Game/Ricefield/Lib/Sawah/Subarea_sawah.gd
@@ -3,11 +3,89 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 
+var padisSmall = load("res://Game/Ricefield/Assets/item_bg_padi/padis_kecil.png")
+var padisMedium = load("res://Game/Ricefield/Assets/item_bg_padi/padis_sedang.png")
+var padisHigh = load("res://Game/Ricefield/Assets/item_bg_padi/padis_besar.png")
+
+@export var GROWTH_SPEED : float = 1
+var PADIS_SIZE = PADIS_SIZES.NONE
+@export var SAWAH_STATE = SAWAH_STATES.GROWTH 
+
+signal sawah_change_state(state)
+
+
+enum PADIS_SIZES {
+	NONE,
+	SMALL,
+	MEDIUM,
+	HIGH
+}
+
+enum SAWAH_STATES {
+	IDLE,
+	PLOW,
+	READY_TO_HARVEST,
+	GROWTH,
+}
+
+var TIME_ELAPSED = 0
 
 func _ready():
-	pass # Replace with function body.
+	set_padis_texture()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+		match SAWAH_STATE :
+			SAWAH_STATES.IDLE :
+				pass
+			SAWAH_STATES.GROWTH:
+				TIME_ELAPSED += delta * GROWTH_SPEED
+				set_padis_growth()
+			SAWAH_STATES.PLOW :
+				pass
+			SAWAH_STATES.READY_TO_HARVEST :
+				set_sawah_state_ready_to_harvest()
+			
+func set_padis_growth():
+	# Update PADIS_SIZE based on elapsed time
+	if TIME_ELAPSED >= 5 and PADIS_SIZE == PADIS_SIZES.NONE:
+		PADIS_SIZE = PADIS_SIZES.SMALL
+		set_padis_texture()
+
+	elif TIME_ELAPSED >= 30 and PADIS_SIZE == PADIS_SIZES.SMALL:
+		PADIS_SIZE = PADIS_SIZES.MEDIUM
+		set_padis_texture()
+	elif TIME_ELAPSED >= 50 and PADIS_SIZE == PADIS_SIZES.MEDIUM:
+		PADIS_SIZE = PADIS_SIZES.HIGH
+		set_padis_texture()
+	elif TIME_ELAPSED >= 80 and PADIS_SIZE == PADIS_SIZES.HIGH:
+		SAWAH_STATE = SAWAH_STATES.READY_TO_HARVEST
+		TIME_ELAPSED = 0
+
+func set_padis_texture():
+	match PADIS_SIZE:
+		PADIS_SIZES.NONE :
+			$TextureRect.set_texture(null)
+		PADIS_SIZES.SMALL :
+			$TextureRect.set_texture(padisSmall)
+		PADIS_SIZES.MEDIUM :
+			$TextureRect.set_texture(padisMedium)
+		PADIS_SIZES.HIGH :
+			$TextureRect.set_texture(padisHigh)
+
+
+func set_sawah_state_idle():
+	SAWAH_STATE = SAWAH_STATES.IDLE
+	emit_signal("sawah_change_state", SAWAH_STATE)
+
+func set_sawah_state_growth():
+	SAWAH_STATE = SAWAH_STATES.GROWTH
+	emit_signal("sawah_change_state", SAWAH_STATE)
+	
+func set_sawah_state_plow():
+	SAWAH_STATE = SAWAH_STATES.PLOW
+	emit_signal("sawah_change_state", "plow")
+
+func set_sawah_state_ready_to_harvest():
+	emit_signal("sawah_change_state", "ready_to_harvest")
--- Game/Ricefield/Main/ricefield_area.gd
+++ Game/Ricefield/Main/ricefield_area.gd
@@ -8,12 +8,24 @@ var VAR_KUANTITAS_PADI = 0
 var VAR_JUMLAH_HAMA = 0
 var VAR_TINGKAT_PESTISIDA = 0
 
+
 func _ready():
-	pass # Replace with function body.
+	$Subarea_sawah.sawah_change_state.connect(_on_sawah_change_state)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _on_sawah_change_state(state):
+	match state :
+		"growth" : 
+			pass
+		"plow" :
+			print("logic for plow")
+		"ready_to_harvest" :
+			print("logic for harvest")
 
 
